e693b46f531bef3a45b9b27a5765fdbec6a5699f
Samuel Lo <systemx637@gmail.com>
2021-07-09 00:28:27 +0100

    add trans2rune

--- runes.js
+++ runes.js
@@ -1,5 +1,5 @@
 // Runes --> Latin
-function runes(input) {
+function rune2latin(input) {
   var runes = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚻ", "ᚾ", "ᛁ", "ᛄ", "ᛇ", "ᛈ", "ᛉ", "ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛝ", "ᛞ", "ᛟ", "ᚪ", "ᚫ", "ᚣ", "ᛠ", "ᛣ", "ᚸ"]
   var latin = ["f", "v", "þ", "o", "r", "č", "ǧ", "w", "h", "n", "i", "y", "u", "p", "z", "s", "t", "b", "e", "m", "l", "ŋ", "d", "œ", "å", "a", "y", "æ", "k", "g"]
 
@@ -19,6 +19,38 @@ function runes(input) {
       }
     }
     if (!assigned)
+      output += input[i];
+  }
+  return output;
+}
+
+// Transcription --> Runes
+function trans2rune(input) {
+  var transcription = ["a", "å", "b", "c", "d", "ð", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "q̇", "r", "s", "t", "u", "v", "w", "ʷ", "x", "y", "ʸ", "z", "þ", "́"]
+  var runes = ["ᚫ", "ᚪ", "ᛒ", "ᚳ", "ᛞ", "ᚧ", "ᛖ", "ᚠ", "ᚸ", "ᚻ", "ᛁ", "ᚷ", "ᛣ", "ᛚ", "ᛗ", "ᚾ", "ᚩ", "ᛈ", "ᚢ", "ᚤ", "ᚱ", "ᛋ", "ᛏ", "ᛇ", "ᚡ", "ᚹ", "ᚹ", "ᛋᚳ", "ᛄ", "ᛄ", "ᛉ", "ᚦ", "̇"]
+
+  var symbols = /[.,\/#!$%\^&\*;:{}=\-_`~() ]/;
+  var output = ""; // initialize output
+
+  var words = input.split();
+
+  for (i = 0; i < input.length; i++) {
+    var assigned = false;
+    for (j = 0; j < transcription.length; j++) {
+      if (!assigned) {
+        if (input[i] == transcription[j]) { // if the letter matches one in the alphabet
+          if (input[i] == "q")
+            if (input[i+1] == "̇")
+              output += "ᚤ";
+            else
+              output += "ᚢ";
+          else
+            output += runes[j];
+          assigned = true;
+        }
+      }
+    }
+    if (!assigned && input[i] !== "̇")
       output += input[i];
   }
   return output;
