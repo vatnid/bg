aec3500c6ba8c7a7089ba8f05d4118217ff6dc27
Samuel Lo <systemx637@gmail.com>
2021-07-18 00:42:49 +0100

    optimise code

--- runes.js
+++ runes.js
@@ -1,24 +1,15 @@
 // Runes --> Latin
 function rune2latin(input) {
-  var runes = ["ᚠ", "ᚢ", "ᚦ", "ᚩ", "ᚱ", "ᚳ", "ᚷ", "ᚹ", "ᚻ", "ᚾ", "ᛁ", "ᛄ", "ᛇ", "ᛈ", "ᛉ", "ᛋ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛝ", "ᛞ", "ᛟ", "ᚪ", "ᚫ", "ᚣ", "ᛠ", "ᛣ", "ᚸ"]
-  var latin = ["f", "v", "þ", "o", "r", "č", "ǧ", "w", "h", "n", "i", "y", "u", "p", "z", "s", "t", "b", "e", "m", "l", "ŋ", "d", "œ", "å", "a", "y", "æ", "k", "g"]
+
+  var r2l = {"ᚠ": "f", "ᚢ": "v", "ᚦ": "þ", "ᚩ": "o", "ᚱ": "r", "ᚳ": "č", "ᚷ": "ǧ", "ᚹ": "w", "ᚻ": "h", "ᚾ": "n", "ᛁ": "i", "ᛄ": "y", "ᛇ": "u", "ᛈ": "p", "ᛉ": "z", "ᛋ": "s", "ᛏ": "t", "ᛒ": "b", "ᛖ": "e", "ᛗ": "m", "ᛚ": "l", "ᛝ": "ŋ", "ᛞ": "d", "ᛟ": "œ", "ᚪ": "å", "ᚫ": "a", "ᚣ": "y", "ᛠ": "æ", "ᛣ": "k", "ᚸ": "g"}
 
   var symbols = /[.,\/#!$%\^&\*;:{}=\-_`~() ]/;
   var output = ""; // initialize output
 
-  var words = input.split();
-
   for (i = 0; i < input.length; i++) {
-    var assigned = false;
-    for (j = 0; j < runes.length; j++) {
-      if (!assigned) {
-        if (input[i] == runes[j]) { // if the letter matches one in the alphabet
-          output += latin[j];
-          assigned = true;
-        }
-      }
-    }
-    if (!assigned)
+    if (input[i] in r2l)
+      output += r2l[input[i]];
+    else
       output += input[i];
   }
   return output;
@@ -26,31 +17,19 @@ function rune2latin(input) {
 
 // Transcription --> Runes
 function trans2rune(input) {
-  var transcription = ["a", "å", "b", "c", "d", "ð", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ŋ", "o", "p", "q", "q̇", "r", "s", "t", "u", "v", "w", "ʷ", "x", "y", "ʸ", "z", "þ", "́"]
-  var runes = ["ᚫ", "ᚪ", "ᛒ", "ᚳ", "ᛞ", "ᚧ", "ᛖ", "ᚠ", "ᚸ", "ᚻ", "ᛁ", "ᚷ", "ᛣ", "ᛚ", "ᛗ", "ᚾ", "ᛝ", "ᚩ", "ᛈ", "ᚢ", "ᚤ", "ᚱ", "ᛋ", "ᛏ", "ᛇ", "ᚥ", "ᚹ", "ᚹ", "ᛋᚳ", "ᛄ", "ᛄ", "ᛉ", "ᚦ", "̇"]
+
+  var t2r = {"a": "ᚫ", "å": "ᚪ", "b": "ᛒ", "c": "ᚳ", "d": "ᛞ", "ð": "ᚧ", "e": "ᛖ", "f": "ᚠ", "g": "ᚸ", "h": "ᚻ", "i": "ᛁ", "j": "ᚷ", "k": "ᛣ", "l": "ᛚ", "m": "ᛗ", "n": "ᚾ", "ŋ": "ᛝ", "o": "ᚩ", "p": "ᛈ", "q": "ᚢ", "q̇": "ᚤ", "r": "ᚱ", "s": "ᛋ", "t": "ᛏ", "u": "ᛇ", "v": "ᚥ", "w": "ᚹ", "ʷ": "ᚹ", "x": "ᛋᚳ", "y": "ᛄ", "ʸ": "ᛄ", "z": "ᛉ", "þ": "ᚦ", "́": "̇", "̇": ""}
 
   var symbols = /[.,\/#!$%\^&\*;:{}=\-_`~() ]/;
   var output = ""; // initialize output
 
-  var words = input.split();
-
   for (i = 0; i < input.length; i++) {
-    var assigned = false;
-    for (j = 0; j < transcription.length; j++) {
-      if (!assigned) {
-        if (input[i] == transcription[j]) { // if the letter matches one in the alphabet
-          if (input[i] == "q")
-            if (input[i+1] == "̇")
-              output += "ᚤ";
-            else
-              output += "ᚢ";
-          else
-            output += runes[j];
-          assigned = true;
-        }
-      }
-    }
-    if (!assigned && input[i] !== "̇")
+    if (input[i] in t2r)
+      if (input[i] == "q" && input[i+1] == "̇")
+        output += "ᚤ";
+      else
+        output += t2r[input[i]];
+    else
       output += input[i];
   }
   return output;
